add2742550b756af10370a3792cb2ba22a85b1dc
raihanazamsyah <raihanazamsyah@gmail.com>
2023-01-29 22:32:31 +0700

    29/01/2023 22:32

--- src/components/homepage/Categories.vue
+++ src/components/homepage/Categories.vue
@@ -1,17 +1,26 @@
 <script setup>
-import CategoriesCard from './../CategoriesCard.vue'
+import { ref } from "vue";
+import CategoriesCard from "./../CategoriesCard.vue";
 
-
+const categories = ref([
+  { id: 1, title: "Mobile UI Kit", count: 731, image: "categories-1.jpg" },
+  { id: 2, title: "Fonts", count: 651, image: "categories-2.jpg" },
+  { id: 3, title: "Icon Set", count: 83559, image: "categories-3.jpg" },
+  { id: 4, title: "Web UI Kit", count: 4500, image: "categories-4.jpg" },
+]);
 </script>
 
 <template>
-    <div class="container px-4 mx-auto my-16 md:px-12">
-        <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">Top Categories</h2>
-        <div class="flex flex-wrap -mx-1 lg:-mx-4">
-            <CategoriesCard title="Mobile UI Kit" :count="731" image="categories-1.jpg" />
-            <CategoriesCard title="Fonts" :count="657" image="categories-2.jpg" />
-            <CategoriesCard title="Icon Set" :count="83559" image="categories-3.jpg" />
-            <CategoriesCard title="Web UI Kit" :count="4500" image="categories-4.jpg" />
-        </div>
+  <div class="container px-4 mx-auto my-16 md:px-12">
+    <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">Top Categories</h2>
+    <div class="flex flex-wrap -mx-1 lg:-mx-4">
+      <CategoriesCard
+        v-for="category in categories"
+        :key="category.id"
+        :title="category.title"
+        :count="category.count"
+        :image="category.image"
+      />
     </div>
+  </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
+import RegisterView from "../views/RegisterView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,11 +11,16 @@ const router = createRouter({
       name: "home",
       component: HomeView,
     },
-    // {
-    //   path: '/categories',
-    //   name: 'categories,
-    //   component: ''
-    // }
+    {
+      path: "/login",
+      name: "login",
+      component: LoginView,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: RegisterView,
+    },
     // {
     //   path: '/about',
     //   name: 'about',
