20830ce29838fbd626054fec2711412cbc7bf869
raihanazamsyah <raihanazamsyah@gmail.com>
2023-01-30 22:37:18 +0700

    30-01-2023  22:37

---
+++ src/components/authentication/FeatureLists.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Customizable from "./features/Customizable.vue";
+import Fortune from "./features/Fortune.vue";
+import Documentation from "./features/Documentation.vue";
+</script>
+
+<template>
+  <Customizable />
+  <Fortune />
+  <Documentation />
+</template>
--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -1,27 +1,18 @@
 <script setup>
-import Logo from './Logo.vue'
-import NavigationLinks from './NavigationLinks.vue'
-import UserInfo from './UserInfo.vue'
-
+import Logo from "./Logo.vue";
+import NavigationLinks from "./NavigationLinks.vue";
+import UserInfo from "./UserInfo.vue";
 </script>
 
 <template>
-    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-        <div class="container flex flex-wrap items-center justify-between mx-auto my-2">
-            <Logo />
-            <NavigationLinks />
-            <!-- <UserInfo /> -->
-            <!-- <div class="md:order-2">
-                <a href="login.html"
-                    class="px-8 py-3 mt-2 mr-2 text-base font-medium text-black bg-gray-200 border border-transparent rounded-full hover:bg-gray-300 md:py-2 md:text-sm md:px-8 hover:shadow">
-                    Sign In
-                </a>
-                <a href="register.html"
-                    class="px-8 py-3 text-base font-medium text-white border border-transparent rounded-full bg-navy hover:bg-navy md:py-2 md:text-sm md:px-8 hover:shadow">
-                    Sign Up
-                </a>
-            </div> -->
-
-        </div>
-    </nav>
+  <nav
+    class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800"
+  >
+    <div
+      class="container flex flex-wrap items-center justify-between mx-auto my-2"
+    >
+      <Logo />
+      <NavigationLinks />
+    </div>
+  </nav>
 </template>
